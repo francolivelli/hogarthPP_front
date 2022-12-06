3de54d5061f9fbf48202f86ff8af4ff584ea9cec
nataliamachella <nataliamachella@gmail.com>
2022-12-06 18:57:20 -0300

    footer con responsive design

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -17,17 +17,28 @@ const Footer = () => {
   }, []);
 
   return (
-    <Flex h="370px" zIndex="-1" alignItems="center" justifyContent="center">
+    <Flex
+      h="370px"
+      zIndex="-1"
+      display={{ sm: "flex", md: "block" }}
+      flexDirection={{ sm: "row", md: "column" }}
+    >
       <Flex h="300px" w="1250px" padding="35px auto" flexWrap="wrap">
-        <Box maxW="md" margin="0px 40px" h="193px" w="158px">
+        <Box
+          maxW="md"
+          margin="0px 40px"
+          h="193px"
+          w="158px"
+          display={{ sm: "none", md: "block" }}
+        >
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREjG76TkFsEFWxICkjW5BVijVD82Wz8hZTBDkW7diyleNt_wm6ZX5Cs9CLEWWPIAnJdDQ&usqp=CAU"
             alt="revista"
           />
         </Box>
-        <Box maxW="md" bg="white" margin="20px 35px" w="356px" h="193px">
+        <Box bg="white" margin="20px 35px" w="356px" h="193px">
           <Link href="/">
-            <img src="/assets/wow.png" alt="logo-wow" h="50px" w="200px" />
+            <img src="/assets/wow.png" alt="logo-wow" h="50px" w="155" />
           </Link>
 
           <Center justifyContent="space-around">
@@ -94,7 +105,7 @@ const Footer = () => {
         <Divider />
         <Flex w="1250px" h="40px">
           <Stack
-            direction="row"
+            direction={{ sm: "column", md: "row" }}
             h="20px"
             p={4}
             marginLeft="250px"
@@ -108,7 +119,12 @@ const Footer = () => {
             <Divider orientation="vertical" />
             <Text fontSize="11px">Preguntas Frecuentes</Text>
           </Stack>
-          <Stack direction="row" h="20px" p={4} marginRight="40px">
+          <Stack
+            direction={{ sm: "column", md: "row" }}
+            h="20px"
+            p={4}
+            marginRight="40px"
+          >
             <Divider orientation="vertical" />
 
             <Text fontSize="11px">Copyright 2022 SA LA NACIÓN </Text>
